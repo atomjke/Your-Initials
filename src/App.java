f4b2f986d887e2569bd510a76a79aaecb1df935c
Evgeny Turmy <jenqa29@mail.ru>
2021-11-07 09:11:37 +0200

    Project added

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("EEEEE  Y   Y  TTTTT");
+        System.out.println("E       Y Y     T  ");
+        System.out.println("E        Y      T  ");
+        System.out.println("EEE      Y      T  ");
+        System.out.println("E        Y      T  ");
+        System.out.println("E        Y      T  ");
+        System.out.println("EEEEE    Y      T  ");
+    }
+}
